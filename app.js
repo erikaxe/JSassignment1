b393adc33a439d66fbcf86eb892c7506be9d213e
eriaxe <erikio93@hotmail.com>
2020-11-11 17:45:49 +0100

    Added keyup event problem with submit event border

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
 
+const taskForm = document.querySelector('.form')
 const inputName = document.querySelector('.input-name');
 const inputCourse = document.querySelector('.input-course');
 const inputAuthor = document.querySelector('.input-author');
 const btn = document.querySelector('.submit-btn');
-const taskForm = document.querySelector('.form')
+
 const list = document.querySelector('.card-section')
 
 
-/* Add input values and create new card in card-section */
-taskForm.addEventListener('submit', function(e){
+/* Add input values and create new card in card-section + border changes on submit event */
+taskForm.addEventListener('submit', function(e){ /* OBS: green border blir kvar efter submit eventet, keyup eventet verkar bättre, kolla upp! */
     e.preventDefault()
     const inputName = document.querySelector('.input-name')
     const inputCourse = document.querySelector('.input-course')
     const inputAuthor = document.querySelector('.input-author')
+    const input = document.querySelector('.input')
     console.log(inputName.value)
     
     if(inputName.value == ''){
-        alert('you need text')
+        /* alert('you need text') */
+        inputName.style.border = '3px solid red';
     }
     if(inputCourse.value == ''){
-        alert('you need text')
+        /* alert('you need text') */
+        inputCourse.style.border = '3px solid red';
     }
     if(inputAuthor.value == ''){
-        alert('you need text')
+        /* alert('you need text') */
+        inputAuthor.style.border = '3px solid red';
     } 
     else {
         const todos = []
@@ -53,4 +58,39 @@ taskForm.addEventListener('submit', function(e){
         inputAuthor.value = ''
     }
     
+})
+
+taskForm.addEventListener('keyup',function(e){
+    const inputName = document.querySelector('.input-name')
+    const inputCourse = document.querySelector('.input-course')
+    const inputAuthor = document.querySelector('.input-author')
+    if(inputName.value == ''){
+        /* alert('you need text') */
+        inputName.style.border = '3px solid red';
+    }
+    else if(inputName.value > 0){
+        inputName.style.border = '3px solid green';
+    }
+    if(inputCourse.value == ''){
+        /* alert('you need text') */
+        inputCourse.style.border = '3px solid red';
+    }
+    else if(inputCourse.value > 0){
+        inputCourse.style.border = '3px solid green';
+    }
+    if(inputAuthor.value == ''){
+        /* alert('you need text') */
+        inputAuthor.style.border = '3px solid red';
+    }
+    else if(inputAuthor.value > 0){
+        inputAuthor.style.border = '3px solid green';
+    }
+    
+    else {
+        list.innerHTML = list.innerHTML + htmlCode
+        inputName.value = ''
+        inputCourse.value = ''
+        inputAuthor.value = ''
+    }
+
 })
